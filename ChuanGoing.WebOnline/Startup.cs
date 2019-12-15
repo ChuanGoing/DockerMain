20557f29cd2bc196da8f27d7282601b4117dfbf5
e370101152 <e370101152@aliyun.com>
2019-12-15 10:26:09 +0800

    测试CI/CD

--- ChuanGoing.WebOnline/Startup.cs
+++ ChuanGoing.WebOnline/Startup.cs
@@ -24,7 +24,7 @@ namespace ChuanGoing.WebOnline
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.MinimumSameSitePolicy = SameSiteMode.None; 
             });
             
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
